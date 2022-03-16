b5286d3b9fa77ddc259643b11e8065d573319617
JeyssonXD <jeyssonelrobinshut@gmail.com>
2022-03-15 22:58:00 -0600

    Creación del proyecto base  con arquitectura MVC 5

---
+++ OTProyect/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OTProyect.MvcApplication" Language="C#" %>
---
+++ OTProyect/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OTProyect.Startup))]
+namespace OTProyect
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
