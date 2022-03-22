be0a482f84add56ff3dbdfb36b59573ef281e672
JeyssonXD <jeyssonelrobinshut@gmail.com>
2022-03-21 22:38:59 -0600

    Configuración de archivos css y js al precargar en el bundles

--- OTProyect/App_Start/BundleConfig.cs
+++ OTProyect/App_Start/BundleConfig.cs
@@ -8,24 +8,24 @@ namespace OTProyect
         // Para obtener más información sobre Bundles, visite http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
-
-            // Utilice la versión de desarrollo de Modernizr para desarrollar y obtener información. De este modo, estará
-            // preparado para la producción y podrá utilizar la herramienta de compilación disponible en http://modernizr.com para seleccionar solo las pruebas que necesite.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
-
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+            //css
+            bundles.Add(new StyleBundle("~/bundles/css").Include(
+                        "~/assets/vendors/iconfonts/mdi/css/materialdesignicons.min.css",
+                        "~/assets/vendors/iconfonts/ionicons/dist/css/ionicons.css",
+                        "~/assets/vendors/iconfonts/flag-icon-css/css/flag-icon.min.css",
+                        "~/assets/vendors/css/vendor.bundle.base.css",
+                        "~/assets/vendors/css/vendor.bundle.addons.css",
+                        "~/assets/css/shared/style.css",
+                        "~/assets/css/demo_1/style.css",
+                        "~/assets/images/favicon.ico"));
+            
+            //plugin js
+            bundles.Add(new ScriptBundle("~/bundles/js").Include(
+                      "~/assets/vendors/js/vendor.bundle.base.js",
+                      "~/assets/vendors/js/vendor.bundle.addons.js",
+                      "~/assets/js/shared/off-canvas.js",
+                      "~/assets/js/shared/misc.js",
+                      "~/assets/js/demo_1/dashboard.js"));
         }
     }
 }
