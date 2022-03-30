2223a4cdecabb243a96a9d279bd8a010c8308a9f
tatilargaespadap <tatgiss@gmail.com>
2022-03-30 14:34:48 -0600

    Cambio Controlador de reporte

--- OTProyect/Controllers/ReporteController.cs
+++ OTProyect/Controllers/ReporteController.cs
@@ -1,4 +1,11 @@
-﻿namespace OTProyect.Controllers
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace OTProyect.Controllers
 {
     /// <summary>
     /// Controlador especifico para Repostes
