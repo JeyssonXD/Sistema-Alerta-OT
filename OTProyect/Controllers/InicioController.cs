183ba350f457a02035e8472b53f9c688e92017e9
tatilargaespadap <tatgiss@gmail.com>
2022-04-20 16:33:02 -0600

    Modificación svg de menu en layout, maquetado de inicio.cshtml

---
+++ OTProyect/Controllers/InicioController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OTProyect.Controllers
+{
+    
+    [Authorize(Roles = "Consultor")]
+    public class InicioController : Controller
+    {
+        // GET: Inicio
+        public ActionResult Inicio()
+        {
+            return View("Inicio");
+        }
+    }
+}
